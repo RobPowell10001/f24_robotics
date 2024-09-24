e445dc766926fe0f6e3a474e627038985ab9ed20
Robert Powell <repowell1@crimson.ua.edu>
2024-09-24 16:31:41 -0500

    decreased stuck tolerance

--- webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
+++ webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
@@ -97,7 +97,7 @@ class WallFollow(Node):
             diffY = math.fabs(float(self.positionLog[-3]) - posy)
 
             # self.get_logger().info("x and y difs are %.3f and %.3f" % (diffX, diffY))
-            if diffX < 0.01 and diffY < 0.01 and self.unstuck == 0:
+            if diffX < 0.007 and diffY < 0.007 and self.unstuck == 0:
                 self.stuck_count += 1
             else: self.stuck_count = 0
             if self.stuck_count >= 500:
